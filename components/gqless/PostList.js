861c8738d036d808c4464d10730357c95eafed38
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-04-10 15:43:30 -0400

    Improve UX re "loading more" state (at cost of DX)

--- components/gqless/PostList.js
+++ components/gqless/PostList.js
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import { graphql, useVariable } from '@gqless/react'
 import { useGqless } from '../../lib/gqless'
 import PostUpvoter from '../gqless/PostUpvoter'
-
-const flat = input => [].concat(...input)
-const flatMap = (array, callback) => flat(array.map(callback))
+import Suspense from '../../lib/SsrCompatibleSuspense'
 
 export const getAllPostsArgs = pageNumber => ({
   skip: pageNumber * 10,
@@ -16,22 +14,18 @@ const PostList = () => {
   const {query} = useGqless()
   const [numberOfPages, setNumberOfPages] = useState(1)
   const loadAnotherPage = () => setNumberOfPages(n => n + 1)
-  const posts = flatMap(
-    Array.from({length: numberOfPages}),
-    (_, index) => query.allPosts(getAllPostsArgs(index))
-  )
-  const areMorePosts = posts.length < query._allPostsMeta.count
+  const postPages = Array.from({length: numberOfPages})
+    .map((_, index) => query.allPosts(getAllPostsArgs(index)))
+  const numberOfVisiblePosts = postPages.reduce((subtotal, posts) => subtotal + posts.length, 0)
+  const totalNumberOfPosts = query._allPostsMeta.count
+  const areMorePosts = numberOfVisiblePosts < totalNumberOfPosts
   return (
     <section>
       <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <div>
-              <span>{index + 1}. </span>
-              <a href={post.url}>{post.title}</a>
-              <PostUpvoter post={post}/>
-            </div>
-          </li>
+        {postPages.map((posts, pageNumber) => (
+          <Suspense key={pageNumber} fallback={<div>Loading more...</div>}>
+            <PostPage pageNumber={pageNumber} posts={posts}/>
+          </Suspense>
         ))}
       </ul>
       {areMorePosts && (
@@ -42,25 +36,6 @@ const PostList = () => {
       <style jsx>{`
         section {
           padding-bottom: 20px;
-        }
-        li {
-          display: block;
-          margin-bottom: 10px;
-        }
-        div {
-          align-items: center;
-          display: flex;
-        }
-        a {
-          font-size: 14px;
-          margin-right: 10px;
-          text-decoration: none;
-          padding-bottom: 0;
-          border: 0;
-        }
-        span {
-          font-size: 14px;
-          margin-right: 5px;
         }
         ul {
           margin: 0;
@@ -80,4 +55,40 @@ const PostList = () => {
     </section>
   )
 };
+
+const PostPage = graphql(({pageNumber, posts}) => {
+  return (<>
+    {posts.map((post, index) => (
+      <li key={index}>
+        <div>
+          <span>{(pageNumber * 10) + index + 1}. </span>
+          <a href={post.url}>{post.title}</a>
+          <PostUpvoter post={post}/>
+        </div>
+      </li>
+    ))}
+    <style jsx>{`
+      li {
+        display: block;
+        margin-bottom: 10px;
+      }
+      div {
+        align-items: center;
+        display: flex;
+      }
+      span {
+        font-size: 14px;
+        margin-right: 5px;
+      }
+      a {
+        font-size: 14px;
+        margin-right: 10px;
+        text-decoration: none;
+        padding-bottom: 0;
+        border: 0;
+      }
+    `}</style>
+  </>)
+})
+
 export default graphql(PostList)
